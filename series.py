42d3c2d4eb8cfe4ca2d00194a784e5b0b951da4c
David Smith <dbsmith.dbs83@gmail.com>
2016-08-08 20:14:19 -0700

    iterative and recursive fibonacci functions

---
+++ series.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+
+def fibonacci_iter(n):
+    series = [0, 1]
+    i = 2
+    try:
+        n = int(n)
+    except:
+        print(u"Please enter a valid integer.")
+        return -1
+    finally:
+        while n > i:
+            series.append(series[i-1] + series[i-2])
+            i += 1
+        return series[n-1]
+
+
+def fibonacci_rec(n, series=[0, 1]):
+    try:
+        n = int(n)
+    except:
+        print(u"Please enter a valid integer.")
+        return -1
+    finally:
+        if n < len(series):
+            return series[n-1]
+        else:
+            series.append(series[len(series) - 1] + series[len(series) - 2])
+            return fibonacci_rec(n, series)
