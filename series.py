bf0117892e04f549ee0fd93a8971e41e01cc1341
David Smith <dbsmith.dbs83@gmail.com>
2016-08-08 20:57:15 -0700

    refactor

--- series.py
+++ series.py
@@ -3,16 +3,14 @@ from __future__ import print_function
 
 def fibonacci_iter(n):
     series = [0, 1]
-    i = len(series)
     try:
         n = int(n)
     except:
         print(u"Please enter a valid integer.")
         return -1
     finally:
-        while n > i:
-            series.append(series[i-1] + series[i-2])
-            i += 1
+        while n > len(series):
+            series.append(series[len(series)-1] + series[len(series)-2])
         return series[n-1]
 
 
